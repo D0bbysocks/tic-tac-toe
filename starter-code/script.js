e3692e865fb34388e36da2d08318f5031b6f6c73
D0bbysocks <214596715+D0bbysocks@users.noreply.github.com>
2026-02-19 09:40:23 +0100

    cpu game init

--- starter-code/script.js
+++ starter-code/script.js
@@ -7,6 +7,17 @@ const gameBoardCells = document.querySelectorAll(".game-board__cell");
 const gameBoardCellIcons = document.querySelectorAll(".game-board__cell-icon");
 const turnIndicator = document.querySelectorAll(".icons");
 
+const quitBtn = document.querySelector('[data-action="quit"]');
+const nextRoundBtn = document.querySelector('[data-action="next-round"]');
+const winningScreen = document.querySelector(".winning-screen");
+const backdrop = document.querySelector(".backdrop");
+const winningScreenPlayerMark = document.querySelector(".winning-screen__player-mark");
+const winningIconX = document.querySelector('.winning-message-icon[data-mark="x"]');
+const winningIconO = document.querySelector('.winning-message-icon[data-mark="o"]');
+
+const playerOneScore = document.querySelector(".score--player .score__points");
+const playerTwoScore = document.querySelector(".score--opponent .score__points");
+const tiesScore = document.querySelector(".score--ties .score__points");
 
 
 let playerX = true;
@@ -14,18 +25,19 @@ let playerClass = "active-x";
 let mode = "cpu";
 let currentScreen = startScreen;
 
-let gameBoard = [
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0]
-];
+let playerOne = "X"; // Spieler 1 immer Mensch
+let playerTwo = "O"; // Spieler 2 CPU oder Mensch
+
 
 let playerXScore = 0;
 let playerOScore = 0;
-let tiesScore = 0;
+let tiesCounter = 0;
+let winner = false;
 
 let movesX = []
 let movesO = []
+
+let gameBoardMoves = 0;
 
 let winningCombinations = [
     [0, 1, 2],
@@ -40,26 +52,27 @@ let winningCombinations = [
 
 
 
-function playerChoice(value) {
-    playerX = value === "x";
-    playerClass = playerX ? "active-x" : "active-o";
+
+
+
+function isHumanTurn() {
+    if (playerOne === "X") return playerX;
+    return !playerX;
 }
+
 
 function selectGameMode(e) {
-    mode = (e === "cpu") ? cpuGameMode() : pvpGameMode();
-    playersTurn();
+    mode = (e === "cpu") ? "cpu" : "pvp";
+    // playersTurn();
 }
 
 
-function cpuGameMode() {
 
-    return "cpu";
+function cpuMove() {
+    console.log("CPU Move");
 }
 
-function pvpGameMode() {
 
-    return "pvp";
-}
 
 function checkWin(player) {
     const hasWon = winningCombinations.some((winCell) => {
@@ -67,27 +80,69 @@ function checkWin(player) {
             return player.includes(cell);
         });
     });
+    return hasWon;
+}
 
-    if (hasWon) {
-        console.log("Winner");
+function endRound(result) {
+    winner = true;
+
+    if (result === "win") {
+        if (playerX) {
+            playerXScore++;
+            playerOneScore.textContent = playerXScore;
+        } else {
+            playerOScore++;
+            playerTwoScore.textContent = playerOScore;
+        }
+
+        setWinner("win");
+
+    } else if (result === "tie") {
+            tiesCounter++;
+            tiesScore.textContent = tiesCounter;
+            setWinner("tie");
+        }       
+
+    winningScreen.hidden = false;
+    backdrop.hidden = false;
+}
+
+
+function checkTie() {
+    if (gameBoardMoves === 9) return true;
+    return false;
+}
+
+function setWinner(result) {
+    if (result === "tie") {
+        winningScreen.querySelector(".winning-screen__tie").removeAttribute("hidden");
+        winningScreen.querySelector(".winning-screen__winner").setAttribute("hidden", true);
+    } else {
+        winningScreen.querySelector(".winning-screen__winner").removeAttribute("hidden");
+        winningScreen.querySelector(".winning-screen__tie").setAttribute("hidden", true);
+        winningScreen.querySelector(".winning-screen__player-mark").textContent = playerX ? "X" : "O";
+        // Show correct icon and hide the other
+        if (playerX === true) {
+            winningScreen.querySelector('.winning-message-icon[data-mark="x"]').removeAttribute("hidden");
+            winningScreen.querySelector('.winning-message-icon[data-mark="o"]').setAttribute("hidden", true);
+        } else {
+            winningScreen.querySelector('.winning-message-icon[data-mark="x"]').setAttribute("hidden", true);
+            winningScreen.querySelector('.winning-message-icon[data-mark="o"]').removeAttribute("hidden");
+        }
     }
 }
+
 
 function playerChange() {
     playerX = !playerX;
     playerClass = playerX ? "active-x" : "active-o";
 }
 
-function playerMove(index) {
+function playerMove(index) {   
     if (playerX) {
         movesX.push(index);
-        console.log(movesX);
-        checkWin(movesX);
-
     } else {
         movesO.push(index);
-        console.log(movesO);
-        checkWin(movesO);
     }
 }
 
@@ -97,7 +152,7 @@ function playersTurn() {
         let oIcon = icon.querySelector(".icon--o");
         xIcon.setAttribute("hidden", true);
         oIcon.setAttribute("hidden", true);
-        if (playerClass !== "active-x") {
+        if (playerClass === "active-x") {
             xIcon.removeAttribute("hidden");
         } else {
             oIcon.removeAttribute("hidden");
@@ -114,13 +169,45 @@ function showScreen(screenToShow) {
 }
 
 function resetGameBoard() {
+    playerClass = "active-x";
+    playerX = true;
     gameBoardCellIcons.forEach((icon) => {
         icon.classList.remove("active-x");
         icon.classList.remove("active-o");
     });
     movesX = [];
     movesO = [];
+    winningScreen.hidden = true;
+    backdrop.hidden = true;
+    winner = false;
+    gameBoardMoves = 0;
+    playersTurn()
+    applyPlayerChoice("x")
+
 }
+
+function updateScoreboardLabels() {
+    const playerLabel = document.querySelector(".score--player .score__text");
+    const opponentLabel = document.querySelector(".score--opponent .score__text");
+
+    if (mode === "cpu") {
+        playerLabel.textContent = "PLAYER";
+        opponentLabel.textContent = "CPU";
+    } else {
+        playerLabel.textContent = "PLAYER 1";
+        opponentLabel.textContent = "PLAYER 2";
+    }
+}
+
+
+function applyPlayerChoice(value) {
+    playerOne = value === "x" ? "X" : "O";
+    playerTwo = value === "x" ? "O" : "X";
+  
+    selectedMark.forEach((input) => {
+      input.checked = input.value === value;
+    });
+  }
 
 
 backButton.addEventListener("click", () => {
@@ -130,36 +217,65 @@ backButton.addEventListener("click", () => {
 
 gameBoardCells.forEach((cell) => {
     cell.addEventListener("click", () => {
-      const icon = cell.querySelector(".game-board__cell-icon");
-  
+        if (winner) return;
 
-      if (icon.classList.contains("active-x") || icon.classList.contains("active-o")) return;
-  
-      const index = Number(cell.dataset.cellIndex);
-  
-      icon.classList.add(playerClass);
-  
-      playerMove(index);
-      playerChange();
-      playersTurn();
+        const icon = cell.querySelector(".game-board__cell-icon");
+
+        if (icon.classList.contains("active-x") || icon.classList.contains("active-o")) return;
+
+        const index = Number(cell.dataset.cellIndex);
+
+        icon.classList.add(playerClass);
+
+        playerMove(index);
+        gameBoardMoves++;
+
+        const currentMoves = playerX ? movesX : movesO;
+
+        if (checkWin(currentMoves)) {
+            endRound("win");
+            return;
+        }
+
+        if (checkTie()) {
+            endRound("tie");
+            return;
+        }
+
+        playerChange();
+        playersTurn();
     });
-  });
+});
+
 
 
 gameStartButtons.forEach((btn) => {
     btn.addEventListener("click", (e) =>{
         selectGameMode(e.target.value);
         resetGameBoard()
+        updateScoreboardLabels()
         showScreen(gameScreen);
+        if (mode === "cpu" && !winner && !isHumanTurn()) {
+            cpuMove();
+          }
     });
 });
 
 selectedMark.forEach((input) => {
     input.addEventListener("change", (e) => {
-        playerChoice(e.target.value);
+        applyPlayerChoice(e.target.value);
     });
 });
 
+quitBtn.addEventListener("click", () => {
+    showScreen(startScreen);
+    resetGameBoard();
+});
+
+nextRoundBtn.addEventListener("click", () => {
+    resetGameBoard();
+    showScreen(gameScreen);
+});
 
 // Todo
 // Punkte Zählen
